059e9be701032d8cbcc6cb7e44945de0fc8a9858
Sanjarbek Abdullayev <sanjarbekabdullayev992@gmail.com>
2024-07-26 19:41:05 +0500

    first commit

--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from category.models import Category
 from store.models import Product
-
-
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponse
 def store(request, category_slug=None):
     categories = None
     products = None
@@ -11,11 +13,14 @@ def store(request, category_slug=None):
         products = Product.objects.filter(category=categories, is_available=True)
         product_count = products.count()
     else:
-        products = Product.objects.all(is_available=True)
+        products = Product.objects.filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
@@ -24,9 +29,18 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
     context = {
         'single_product': single_product,
+        'in_cart': in_cart
     }
-    return render(request, 'store/product_detail.html')
+    return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if 'keyword':
+            products =Prouct.objects.order_by('-created_date').filter(description__icontains=keyword)
+    return render(request, 'store/store.html')
